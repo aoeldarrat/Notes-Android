65fb0a7d5937459c3840b4114a69283a5fae1a3b
aoeldarrat <abudyd@gmail.com>
2025-08-02 14:40:51 -0500

    Add some navigationin main activity, remove some comments and just general prep for next state to setup note creation

--- app/src/main/java/com/app/notes/MainActivity.kt
+++ app/src/main/java/com/app/notes/MainActivity.kt
@@ -21,25 +21,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.app.notes.ui.theme.NotesTheme
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
-import org.koin.core.module.dsl.bind
-import org.koin.core.module.dsl.singleOf
-import org.koin.core.module.dsl.viewModel
-import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.module
+import kotlinx.serialization.Serializable
 
-class MainActivity : ComponentActivity() {
-
-    // TODO setup navigation to another fragment
-    // TODO setup room database or another local database for notes
-    // Dependency injection is on pause
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Dependency injection - dont need right now
+// Dependency injection - dont need right now
 //        val appModule = module {
 //            singleOf(::NoteRepositoryImpl) { bind<NoteRepository>() }
 //            viewModelOf(::NotesViewModel)
@@ -51,69 +39,96 @@ class MainActivity : ComponentActivity() {
 //            modules(appModule)
 //        }
 
+
+/**
+ * Routes to navigate to
+ */
+@Serializable
+object Home
+
+@Serializable
+object NoteCreation
+
+class MainActivity : ComponentActivity() {
+
+    // TODO setup navigation to another fragment
+    // TODO setup room database or another local database for notes
+    // Dependency injection is on pause
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
-            val addButtonEnabled = remember { mutableStateOf(true) }
-
+            val navController = rememberNavController()
             NotesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        // Header
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Text("Search bar here")
-//                            SearchBar(inputField = {
-//                                TextField()
-//                            }) { }
-
-                            // Add note button
-
-                            Button(
-                                onClick = {
-                                    // Handle button click
-                                },
-                                enabled = addButtonEnabled.value,
-                                colors = ButtonDefaults.buttonColors(
-                                    containerColor = Color.White,
-                                    contentColor = Color.Black,
-                                    disabledContainerColor = Color.Gray,
-                                    disabledContentColor = Color.Black
-                                ),
-                                border = ButtonDefaults.outlinedButtonBorder(),
-                                contentPadding = innerPadding,
-                            ) {
-                                Text("Add note")
+                    NavHost(
+                        modifier = Modifier.padding(innerPadding),
+                        navController = navController,
+                        startDestination = Home
+                    ) {
+                        composable<Home> {
+                            HomeScreen {
+                                navController.navigate(route = NoteCreation)
                             }
                         }
 
-                        // List of notes
-                        LazyColumn {
-                            item {
-                                Text("Note x")
-                            }
+                        composable<NoteCreation> {
+                            NoteCreateScreen()
                         }
                     }
-
                 }
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun HomeScreen(
+    onCreateNote: () -> Unit
+) {
+    val addButtonEnabled = remember { mutableStateOf(true) }
+    Column {
+        // Header
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            // TODO add search bar
+//            SearchBar(inputField = { SearchTextField() }) { }
+
+            Button(
+                onClick = onCreateNote,
+                enabled = addButtonEnabled.value,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                    contentColor = Color.Black,
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.Black
+                ),
+                border = ButtonDefaults.outlinedButtonBorder(),
+            ) {
+                Text("Add note")
+            }
+        }
+
+        // List of notes
+        LazyColumn {
+            item {
+                Text("Note x")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NotesTheme {
-        Greeting("Android")
-    }
+fun NoteCreateScreen() {
+    // TODO Note creation screen
+
+    Text(text = "Hello!",)
 }
